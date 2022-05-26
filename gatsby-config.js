4683cb08c90b4a3956d6ba20b555cdcd8b85780e
Rajeshwar Rathi <rajeshwar.r.rathi@gmail.com>
2022-05-26 16:39:55 +0530

    added google anlaytics for my site

--- gatsby-config.js
+++ gatsby-config.js
@@ -178,9 +178,21 @@ module.exports = {
     `gatsby-plugin-postcss`,
     `gatsby-plugin-feed`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-XBJ3CG2MY0", // Google Analytics / GA
+         ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://rajrathi.tech",
+        },
       },
     },
     {
