248d5fdd055ff70328c9aaa75563c00a3f833ef7
Rajeshwar Rathi <rajeshwar.r.rathi@gmail.com>
2022-08-09 00:12:08 +0530

    Updates

---
+++ component---src-pages-404-jsx-411f0be15e10886b9614.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrrblogger=self.webpackChunkrrblogger||[]).push([[256],{7291:function(e,t,n){n.r(t);var l=n(7294),r=n(1597),u=n(1988),a=n(7110),o="text-lg font-bold",s="underline";t.default=function(){return l.createElement(u.Z,null,l.createElement(a.Z,{title:"Not found"}),l.createElement("h1",{className:o},"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist."," ",l.createElement(r.Link,{className:s,to:"/"},"Return to safety"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-411f0be15e10886b9614.js.map
